9650a1d2dce9e82bf0140642e86296680b3bb90f
Evgenij Subbota <essaturday@gmail.com>
2017-08-31 13:48:14 +0300

    4ое ДЗ (исходник)

---
+++ Homework.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+* Date: 31.08.2017
+* Time: 12:31
+*/
+function setAsIs($name, $ebay_type){
+    if($ebay_type != 'Default') {
+        $name = 'AS IS ' . $name;
+        return $name;
+    } else {
+        return $name;
+    }
+}
