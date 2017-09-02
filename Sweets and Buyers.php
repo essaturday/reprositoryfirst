c768fb37a853b6ec6e69431f0d080d3be82f2ba7
Evgenij Subbota <essaturday@gmail.com>
2017-09-02 12:10:01 +0300

    Test homework

---
+++ Sweets and Buyers.php
@@ -0,0 +1,8 @@
+<?php
+$people = array('buyer1' => 'Baz', 'buyer2' => 'Bag');
+$sweets = array('sweet1' => 'cakes', 'sweet2' => 'cookies', 'sweet3' => 'cherry pie');
+for ($col_ppl = 2; $col_ppl <= 2; $col_ppl++)
+{
+    echo "Hello {$people['buyer1']} and {$people ['buyer2']}! We have lot of sweets in our shop like: {$sweets ['sweet1']}, {$sweets ['sweet2']}
+and {$sweets ['sweet3']}.";
+}
